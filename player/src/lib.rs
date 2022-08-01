c18c49693ea65b5550c4d2a8821e528f3d4311de
kristianwahlroos <kristian.whl@gmail.com>
2022-08-01 18:35:38 +0300

    P1-P6

---
+++ player/src/lib.rs
@@ -0,0 +1,115 @@
+use gdnative::api::AnimationNodeStateMachinePlayback;
+use gdnative::api::AnimationPlayer;
+use gdnative::api::AnimationTree;
+use gdnative::prelude::*;
+
+enum State {
+    Move,
+    Roll,
+    Attack,
+}
+
+const MAX_SPEED: f32 = 80.0;
+const ACCELERATION: f32 = 500.0;
+const FRICTION: f32 = 500.0;
+
+#[derive(NativeClass)]
+#[inherit(KinematicBody2D)]
+struct Player {
+    velocity: Vector2,
+    animation_player: Option<Ref<AnimationPlayer>>,
+    animation_tree: Option<Ref<AnimationTree>>,
+    animation_state: Option<Ref<AnimationNodeStateMachinePlayback>>,
+}
+
+#[methods]
+impl Player {
+    fn new(_owner: &KinematicBody2D) -> Self {
+        Player {
+            velocity: Vector2::ZERO,
+            animation_player: None,
+            animation_tree: None,
+            animation_state: None,
+        }
+    }
+
+    #[godot]
+    fn _ready(&mut self, #[base] owner: &KinematicBody2D) {
+        owner.set_physics_process(true);
+        self.animation_player = unsafe {
+            Some(
+                owner
+                    .get_node_as::<AnimationPlayer>("AnimationPlayer")
+                    .unwrap()
+                    .claim(),
+            )
+        };
+        self.animation_tree = unsafe {
+            Some(
+                owner
+                    .get_node_as::<AnimationTree>("AnimationTree")
+                    .unwrap()
+                    .claim(),
+            )
+        };
+        unsafe {
+            self.animation_state = self
+                .animation_tree
+                .unwrap()
+                .assume_safe()
+                .get("parameters/playback")
+                .to_object::<AnimationNodeStateMachinePlayback>();
+        }
+    }
+
+    #[godot]
+    fn _physics_process(&mut self, #[base] owner: &KinematicBody2D, delta: f32) {
+        let input = Input::godot_singleton();
+        let mut input_vector = Vector2::new(0.0, 0.0);
+
+        input_vector.x = (Input::get_action_strength(input, "ui_right", false)
+            - Input::get_action_strength(input, "ui_left", false)) as f32;
+        input_vector.y = (Input::get_action_strength(input, "ui_down", false)
+            - Input::get_action_strength(input, "ui_up", false)) as f32;
+        if input_vector.x != 0.0 || input_vector.y != 0.0 {
+            unsafe {
+                self.animation_state
+                    .as_ref()
+                    .unwrap()
+                    .assume_safe()
+                    .travel("Run");
+            }
+            input_vector = input_vector.normalized();
+            unsafe {
+                self.animation_tree
+                    .unwrap()
+                    .assume_safe()
+                    .set("parameters/Idle/blend_position", input_vector);
+                self.animation_tree
+                    .unwrap()
+                    .assume_safe()
+                    .set("parameters/Run/blend_position", input_vector);
+            }
+            self.velocity = self
+                .velocity
+                .move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
+        } else {
+            unsafe {
+                self.animation_state
+                    .as_ref()
+                    .unwrap()
+                    .assume_safe()
+                    .travel("Idle");
+            }
+            self.velocity = self.velocity.move_toward(Vector2::ZERO, FRICTION * delta);
+        }
+        self.velocity =
+            owner.move_and_slide(self.velocity, Vector2::ZERO, false, 4, 0.785398, true);
+    }
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<Player>();
+}
+
+godot_init!(init);
